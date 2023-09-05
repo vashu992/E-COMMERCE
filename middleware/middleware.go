274778650d49cf105278d26919832c7be03bdec3
vashu992 <ashutoshpratap992@gmail.com>
2023-09-05 14:56:40 +0530

    middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	token "github.com/vashu992/E-Commerce/tokens"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context){
+		ClientToken := c.Request.Header.Get("token")
+		if ClientToken == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"No authorization header provided"})
+			c.Abort()
+			return
+		}
+		claims, err := token.ValidateToken(ClientToken)
+		if err != ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
