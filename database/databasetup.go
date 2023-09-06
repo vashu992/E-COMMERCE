b7be70dee2cb395e8c623638d4177df9b4686dce
vashu992 <ashutoshpratap992@gmail.com>
2023-09-06 12:15:24 +0530

    databasetup from database

---
+++ database/databasetup.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongo://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb (")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongo")
+	return client
+
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("E-Commerce").Collection(collectionName)
+	return collection
+
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("E-Commerce").Collection(collectionName)
+	return productCollection
+}
